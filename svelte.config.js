b3375e1647c28e3cc96c9ddcaf1bc04907f293ba
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2024-07-18 23:16:25 +0500

    fallback page

--- svelte.config.js
+++ svelte.config.js
@@ -9,10 +9,9 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: '404.html'
+		}),
 		paths: {
             base: process.env.NODE_ENV === 'production' ? '/csi_gen_sim' : '',
         }
@@ -21,3 +20,4 @@ const config = {
 };
 
 export default config;
+
