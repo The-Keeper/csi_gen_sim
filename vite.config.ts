7bb8f87fb6a72b9884d9e3ab58dcbeb7302d2989
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2024-07-20 13:26:41 +0500

    uno works

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import UnoCSS from '@unocss/vite';
-import extractorSvelte from '@unocss/extractor-svelte';
+import UnoCSS from 'unocss/vite';
 
 export default defineConfig({
-	plugins: [UnoCSS({
-		extractors: [
-			extractorSvelte(),
-		],
-		/* more options */
-	}), sveltekit()]
-});
+  plugins: [
+    UnoCSS(),
+    sveltekit(),
+  ],
+})
