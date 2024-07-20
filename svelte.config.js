e35f84b50fc1d8df248bbf0653941f00418a91ea
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2024-07-20 11:09:18 +0500

    svelte unocss

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,11 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 // import { vitePreprocess } from '@sveltejs/kit/vite';
-import UnoCSS from 'unocss/vite'
-import extractorSvelte from '@unocss/extractor-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	plugins: [
-		UnoCSS({
-		  extractors: [
-			extractorSvelte(),
-		  ],
-		  /* more options */
-		}),
-	],
 	preprocess: vitePreprocess(),
 
 	kit: {
