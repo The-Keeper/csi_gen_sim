affe7b821007d4f25a153a85fabf80ba2d876e83
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2025-02-09 00:49:45 +0500

    UI

--- src/routes/reports/+page.svelte
+++ src/routes/reports/+page.svelte
@@ -3,21 +3,29 @@
 	import TextAreaArray from "$lib/components/TextAreaArray.svelte";
 </script>
 
+<div class="flex flex-col gap-4 w-full">
 {#each reports_input as r_input, r_idx }
-	<div>
-		<div class="flex items-center justify-end gap-1">
-			<label class="block" for="weight_min">Название отчёта</label>
-			<input class="block" bind:value={r_input.title} />
+		<div class="flex items-stretch">
+			<div class="flex items-start justify-end gap-1">
 
+				<div class="flex items-end">
+					<div>
+						<label class="block" for="{`title_${r_idx}`}">Название отчёта</label>
+						<input id="{`title_${r_idx}`}" class="block" bind:value={r_input.title} />	
+					</div>
+	
+					<div>
+						<label class="block" for="{`number_${r_idx}`}">Число респондентов</label>
+						<input type="number" min="1" max="1000" bind:value={r_input.respondents_number} />
+					</div>
+				</div>
 
-			<input type="number" min="1" max="1000" bind:value={r_input.respondents_number} />
+			</div>
+			<TextAreaArray bind:values = {r_input.subjects} />	
+			<button onclick={() => remove_report_at_idx(r_idx)}>Удалить отчет</button>
 
-			<button onclick={() => remove_report_at_idx(r_idx)}>Удалить</button>
 		</div>
-		<TextAreaArray bind:values = {r_input.subjects} />	
-	</div>
-{/each}
+	{/each}
 
-<div>
 	<button onclick={add_report}>Добавить отчёт</button>
 </div>
