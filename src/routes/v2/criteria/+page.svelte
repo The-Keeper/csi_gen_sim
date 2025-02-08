1d0622d5f76794c40e640480572aa8222f55ac01
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2025-02-08 19:44:42 +0500

    fill criteria

--- src/routes/v2/criteria/+page.svelte
+++ src/routes/v2/criteria/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { layout } from "$lib/store.svelte";
 	import TextAreaArray from "$lib/components/TextAreaArray.svelte";
+	import CriterionCard from "$lib/components/CriterionCard.svelte";
 
 	let criteria_names = ['Критерий 1'];
 	
@@ -23,3 +24,12 @@
 
 	<button onclick={set_criteria}>Заполнить</button>
 </div>
+
+<details class="w-full md:w-200" open>
+	<summary>Критерии</summary>
+	<div class="flex flex-wrap w-full sm:w-160 gap-2">
+		{#each layout.criteria as criterion, i}
+			<CriterionCard bind:criterion />
+		{/each}
+	</div>
+</details>
