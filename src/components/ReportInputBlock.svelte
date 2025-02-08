9667c80c989fa9101437a0b912e7fdc14fc9584c
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2025-02-08 17:17:26 +0500

    textarea array component

--- src/components/ReportInputBlock.svelte
+++ src/components/ReportInputBlock.svelte
@@ -1,25 +1,13 @@
 <script lang="ts">
+	import TextAreaArray from "./TextAreaArray.svelte";
+
 let { title =  $bindable(''), subjects = $bindable([] as string[]) } = $props();
 
-function setSubjectNames(val: string) {
-	subjects = val
-		.split("\n")
-		.filter((s) => s.trim() !== "")
-		.map((name) => name);
-}
-
-export function getSubjects() {
-	return subjects;
-}
 </script>
 <div class="block">   
     <div class="flex items-center justify-end gap-1">
         <label class="block" for="weight_min">Название отчёта</label>
         <input class="block" bind:value={title} />
     </div>
-    <textarea class="block w-full h-40" id="subject_names" bind:value={
-        () => subjects.join('\n') + ('\n'),
-        setSubjectNames
-    }></textarea>
-
+    <TextAreaArray bind:values = {subjects} />
 </div>
---
+++ src/components/TextAreaArray.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    let { values = $bindable([] as string[]) } = $props();
+    
+    function setValues(val: string) {
+        values = val
+            .split("\n")
+            .filter((s) => s.trim() !== "")
+            .map((name) => name);
+    }
+    
+</script>
+<textarea bind:value={
+    () => values.join('\n') + ('\n'),
+    setValues
+}></textarea>
