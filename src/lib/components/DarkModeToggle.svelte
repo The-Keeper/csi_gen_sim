6d23efd7b50a7596f103353d39ab72890d7d70ff
The-Keeper <6674932+The-Keeper@users.noreply.github.com>
2025-02-09 15:04:48 +0500

    pointer

--- src/lib/components/DarkModeToggle.svelte
+++ src/lib/components/DarkModeToggle.svelte
@@ -19,8 +19,8 @@
     }
 </script>
 {#if browser}
-    <div class="fixed z-90 bottom-10 right-8 bg-blue-600 w-15 h-15 rounded-full drop-shadow-lg flex justify-center items-center text-white text-2xl hover:bg-blue-700 hover:drop-shadow-2xl cursor-pointer"> 
-        <label for="theme">{themeIcon}</label>  
+    <div class="fixed z-90 bottom-10 right-8 bg-blue-600 w-15 h-15 rounded-full drop-shadow-lg flex justify-center items-center text-white text-2xl hover:bg-blue-700 hover:drop-shadow-2xl"> 
+        <label class="cursor-pointer" for="theme">{themeIcon}</label>  
         <input class="hidden" type="checkbox" onchange={changeTheme} bind:checked name="theme" id="theme" />    
     </div>
 {/if}
